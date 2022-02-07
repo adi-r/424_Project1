2591048530e410bd7f2490d3a7b2370a69064427
Aditya Ranganathan <ranga.aditya97@gmail.com>
2022-02-07 17:54:05 -0600

    Added Month names Refactored file

--- project1.R
+++ project1.R
@@ -1,5 +1,5 @@
 # Directory
-setwd('C:\\Users\\aranga22\\Downloads\\Academics\\Sem 2\\424 Visual Data\\Project 1')
+setwd('C:\\Users\\aranga22\\Downloads\\Academics\\Sem 2\\424 Visual Data\\Projects\\424_Project1')
 getwd()
 
 # Libraries
@@ -25,6 +25,27 @@ cta_station$month <- as.numeric(format(cta_station$date, format='%m'))
 cta_station$day <- as.numeric(format(cta_station$date, format='%d'))
 cta_station$week_day <- wday(cta_station$date, label=TRUE)
 
+# Add Month name
+name_of_month <- function(df){
+  df$month_name[df$month==1] <- 'January'
+  df$month_name[df$month==2] <- 'February'
+  df$month_name[df$month==3] <- 'March'
+  df$month_name[df$month==4] <- 'April'
+  df$month_name[df$month==5] <- 'May'
+  df$month_name[df$month==6] <- 'June'
+  df$month_name[df$month==7] <- 'July'
+  df$month_name[df$month==8] <- 'August'
+  df$month_name[df$month==9] <- 'September'
+  df$month_name[df$month==10] <- 'October'
+  df$month_name[df$month==11] <- 'November'
+  df$month_name[df$month==12] <- 'December'
+  
+  return(df)
+  
+}
+cta_station <- name_of_month(cta_station)
+
+# Extract Relevant Station Data
 # UIC
 uic_station <- subset(cta_station, stationname == 'UIC-Halsted')
 glimpse(uic_station)
@@ -51,14 +72,17 @@ station_data <- do.call("rbind", list(uic_station, ohare_station, racine_station
 names(station_data)[1] <- 'station id'
 str(station_data)
 
-# no_of_chunks <- 10
-# f <- ceiling(1:nrow(station_data) / nrow(station_data) * 10)
-# res <- split(station_data, f)
-# map2(res, paste0("chunk_", names(res), ".tsv"), write.csv)
-
+# Write data to .tsv file
 write.table(station_data, file='station_data.tsv', quote=FALSE, sep='\t', col.names = NA)
 
+# Load and verify written data
 trial <- read.table(file="station_data.tsv", sep="\t", header=TRUE, quote="\"", )
 trial <- trial[, -1]
 str(trial)
 
+################################################################################
+# no_of_chunks <- 10
+# f <- ceiling(1:nrow(station_data) / nrow(station_data) * 10)
+# res <- split(station_data, f)
+# map2(res, paste0("chunk_", names(res), ".tsv"), write.csv)
+################################################################################
